ede29bf8f0beb085c0da31a8e752f59d7629dade
wss <2683454510@qq.com>
2022-10-18 16:09:50 +0800

    feat: 添加axios请求

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,39 +4,21 @@
       <el-button type="primary" @click="handleAdd">增加</el-button>
     </div>
 
-    <el-table
-        :data="tableData"
-        style="width: 100%">
-      <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180">
       </el-table-column>
-      <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
+      <el-table-column prop="name" label="姓名" width="180">
       </el-table-column>
-      <el-table-column
-          prop="address"
-          label="地址">
+      <el-table-column prop="address" label="地址">
       </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row.name)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row.name)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-dialog
-        :title="title"
-        :visible.sync="dialogVisible"
-        width="60%">
+    <el-dialog :title="title" :visible.sync="dialogVisible" width="60%">
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="日期" prop="date">
           <el-input v-model="form.date"></el-input>
@@ -86,8 +68,9 @@ export default {
           { required: true, message: '请输入地址', trigger: 'blur' },
         ],
       },
-      title:'添加',
-      active:''
+      title: '添加',
+      active: '',
+      choosePeople:''
     }
   },
   mounted() {
@@ -98,24 +81,56 @@ export default {
       axios.get('/api/search')
     },
     handleAdd() {
+      //打开dialog
+      this.dialogVisible = true
+      this.title = '添加'
     },
-    handleSubmit(formName){
+    async handleSubmit(formName) {
+      if (this.title == '添加') {
+        console.log('添加')
+        axios({ url: '/api/add', data: { form: this.form}, method: 'POST' }).then(()=>{
+          console.log('添加成功')
+        }).catch(()=>{
+          console.log('添加失败')
+        })
+        
+      } else {
+        axios({ url: '/api/edit', data: { form: this.form, id:this.choosePeople}, method: 'POST' }).then(()=>{
+          console.log('编辑成功')
+        }).catch(()=>{
+          console.log('编辑失败')
+        })
+      }
+      this.dialogVisible = false
     },
-    handleEdit(index,data){
+    handleEdit(index, data) {
+      //获取选择的人物 scope.row.name
+      this.choosePeople = data
+      //打开dialog
+      this.dialogVisible = true
+      this.title = '编辑'
     },
-    handleDelete(index,data){
+    async handleDelete(index, data) {
+      //获取选择的人物 scope.row.name
+      this.choosePeople = data
+      //axios请求，传递选择的人id
+      axios({url:'/api/delete',data:{id:this.choosePeople},method:'POST'}).then(()=>{
+        console.log('删除成功')
+      }).catch(()=>{
+        console.log('删除失败')
+      })
     },
 
   }
 }
 </script>
 <style  scoped>
-.add{
+.add {
   overflow: hidden;
   margin-bottom: 24px
-
 }
-.add button{
+
+.add button {
   float: right;
 }
 </style>
